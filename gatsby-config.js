afda4cc1fbd10fa9722fb724b97bfdedbd085a3f
Brian Maier Jr <brimaidesigns@gmail.com>
2020-02-10 22:45:00 -0500

    + google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,6 +36,13 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-30261260-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
